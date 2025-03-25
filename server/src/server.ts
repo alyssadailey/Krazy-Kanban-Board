d0c9d7399c617f41684b7ab32bba6232d6452c5a
alyssadailey <alyssadailey28@gmail.com>
2025-03-25 13:47:16 -0400

    adjusted code to allow render to work

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,17 +4,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import path from 'node:path';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serves static files in the entire client's dist folder
-app.use(express.static('../../client/dist'));
 
 app.use(express.json());
 app.use(routes);
+
+// Serves static files in the entire client's dist folder
+
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
